b4d1903232d64d32b8202a4701e565f1ab64b1d3
venu0807 <pvgreddy3@gmail.com>
2023-10-17 12:06:06 +0530

    Main

--- officer/admin.py
+++ officer/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from officer.models import Team1
+from officer.models import Team1,Team2
 
 class T1admin(admin.ModelAdmin):
     list_display=['id','name','fathername','age','batch','role','contact']
 
+class T2admin(admin.ModelAdmin):
+    list_display=['id','name','fathername','age','batch','role','contact']
+
 admin.site.register(Team1,T1admin)
+admin.site.register(Team2,T2admin)
